4b97d652988393ad002c6d6c4c29a72cde4adb26
David-Chan-Ho2 <dho12@charlotte.edu>
2026-02-23 13:36:11 -0500

    add swr for better client api fetching

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,31 +1,31 @@
 "use client";
 
-import { ILocation } from "@/types/marker.types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import useSWR from "swr";
+
+import { ILocation } from "@/types/marker.types";
 import { CustomIcon } from "./Icon";
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function Map() {
-  const [locations, setLocations] = useState<ILocation[]>([]);
+  const { data, error, isLoading } = useSWR<ILocation[]>(
+    "/api/locations",
+    fetcher,
+  );
   const [uncc, setUncc] = useState<[number, number]>([35.3071, -80.7357]);
 
-  useEffect(() => {
-    const fetchLocations = async () => {
-      const res = await fetch("/api/locations");
-      const data: ILocation[] = await res.json();
-      setLocations(data);
-    };
-
-    fetchLocations();
-  }, []);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
 
   return (
     <MapContainer center={uncc} zoom={16} className="map">
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {locations.map((marker) => (
-        <div key={marker.label}>
-          <Marker position={marker.position} icon={CustomIcon}>
-            <Popup>{marker.label}</Popup>
+      {data?.map((location) => (
+        <div key={location.label}>
+          <Marker position={location.position} icon={CustomIcon}>
+            <Popup>{location.label}</Popup>
           </Marker>
         </div>
       ))}
