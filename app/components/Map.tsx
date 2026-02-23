aea68380da662513a44d2adee8b39ebe1c019bf3
David-Chan-Ho2 <dho12@charlotte.edu>
2026-02-23 09:03:04 -0500

    move map styles to global.css

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -6,15 +6,15 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import { CustomIcon } from './Icon'
 
 export default function Map() {
-  const [uncc, setUncc] = useState<[number, number]>([35.3071, -80.7357])
   const [markers, setMarkers] = useState<IMarker[]>([
     {id: 1, label: "Woodward Hall", position: [35.3071, -80.7357]}, 
     {id: 2, label: "Mebane Hall", position: [35.3073, -80.734]}, 
     {id: 3, label: "University Recreation Center", position: [35.3081, -80.7355]}
   ]);
+  const [uncc, setUncc] = useState<[number, number]>(markers[0].position)
 
   return (
-    <MapContainer center={uncc} zoom={16} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={uncc} zoom={16} className='map'>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {markers.map((marker) => (
             <div key={marker.label}>
